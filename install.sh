7e0f2f0c3e0fe89729d9f73ed74ac12217141b15
Paulo Eduardo Rezende <rezendepauloeduardo@outlook.com>
2020-07-11 21:42:27 -0300

    refactor(install): improve install process

--- install.sh
+++ install.sh
@@ -21,65 +21,64 @@ message() {
     echo "$blue==>$white $bold$content"
 }
 
-message "Configuring BASH settings"
-ln -svf $(pwd)/bash/bashrc $HOME/.bashrc > /dev/null
-
-message "Configuring GIT settings"
-ln -svf $(pwd)/git/gitconfig $HOME/.gitconfig > /dev/null
-
-message "Configuring SSH settings"
-ln -svf $(pwd)/ssh/config $HOME/.ssh/config > /dev/null
-
-message "Downloading Kitty"
-curl -sL https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin > /dev/null
-
-message "Configuring Kitty settings"
-if [[ ! -d ~/.config/kitty ]]
-then
-  message "Creating Kitty folder"
-  mkdir $HOME/.config/kitty
-fi
-ln -svf $(pwd)/kitty/kitty $HOME/.config/kitty/kitty.conf > /dev/null
-
 message "Downloading Homebrew"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
-message "Downloading Neovim"
-brew install neovim > /dev/null
-brew install ripgrep > /dev/null
-brew tap homebrew/cask-fonts > /dev/null
-brew cask install font-hack-nerd-font > /dev/null
+message "Downloading packages"
+brew install git
+brew install neovim
+brew install fzf
+brew install ripgrep
+brew install tmux
+brew install zsh
+
+message "Downloading fonts"
+brew tap homebrew/cask-fonts
+brew cask install font-hack-nerd-font
+
 curl -sL https://download-cf.jetbrains.com/fonts/JetBrainsMono-1.0.3.zip --output $HOME/Downloads/jetbrains-mono.zip
 unzip -d $HOME/Downloads/ $HOME/Downloads/jetbrains-mono.zip > /dev/null
 cp $HOME/Downloads/JetBrainsMono*/**/*.ttf $HOME/Library/Fonts
 
-message "Configuring Neovim settings"
+message "Configuring BASH"
+ln -svf $(pwd)/bash/bashrc $HOME/.bashrc > /dev/null
+
+message "Configuring GIT"
+ln -svf $(pwd)/git/gitconfig $HOME/.gitconfig > /dev/null
+
+message "Configuring SSH"
+ln -svf $(pwd)/ssh/config $HOME/.ssh/config > /dev/null
+
+message "Downloading Kitty Terminal"
+curl -sL https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin > /dev/null
+
+message "Configuring Kitty Terminal"
+if [[ ! -d ~/.config/kitty ]]
+then
+  mkdir $HOME/.config/kitty
+fi
+ln -svf $(pwd)/kitty/kitty.conf $HOME/.config/kitty/kitty.conf > /dev/null
+
+message "Configuring Neovim"
 if [[ ! -d ~/.config/nvim ]]
 then
-  message "Creating Neovim folder"
   mkdir $HOME/.config/nvim
 fi
 ln -svf $(pwd)/neovim/init.vim $HOME/.config/nvim/init.vim > /dev/null
 ln -svf $(pwd)/neovim/coc-settings.json $HOME/.config/nvim/coc-settings.json > /dev/null
 
-message "Downloading Tmux"
+message "Downloading Tmux Plugin Manager"
 brew install tmux > /dev/null
 git clone --quiet https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
-message "Configuring Tmux settings"
-ln -svf $(pwd)/tmux/tmux $HOME/.tmux.conf > /dev/null
-
-message "Downloading NVM"
-curl -s -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash > /dev/null
-source $HOME/.bashrc
-nvm install --lts
+message "Configuring Tmux"
+ln -svf $(pwd)/tmux/tmux.conf $HOME/.tmux.conf > /dev/null
 
 message "Downloading Yarn"
 curl -s -o- -L https://yarnpkg.com/install.sh | bash > /dev/null
 
-message "Downloading Oh My ZSH"
-brew install zsh
+message "Downloading zplug"
 curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
 
-message "Configuring ZSH settings"
+message "Configuring ZSH"
 ln -svf $(pwd)/zsh/zshrc $HOME/.zshrc
