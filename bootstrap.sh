61487eb63d89ae1373c43f6d977e128d9f7ae44d
Paulo Eduardo Rezende <rezendepauloeduardo@outlook.com>
2021-06-20 21:27:59 -0300

    feat: add bash script to bootstrap install

---
+++ bootstrap.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# Exit immediately if a command exits with a non-zero status.
+set -e
+
+# Install ansible if it is not available on the machine
+if ! [ -x "$(command -v ansible)" ]; then
+  sudo apt update
+  sudo apt install software-properties-common
+  sudo add-apt-repository --yes --update ppa:ansible/ansible
+  sudo apt install --yes ansible
+fi
+
+# Check if any role was entered
+tags="all"
+while getopts t: tags
+do
+  case "${tags}" in
+    t) tags=${OPTARG};;
+  esac
+done
+
+ansible-playbook -i ./hosts.yml ./environment.yml --ask-become-pass --tags $tags
