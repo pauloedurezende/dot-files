3f5011ee7958307525ef1730cfde3aee8c6fce3a
Paulo Eduardo Rezende <rezendepauloeduardo@outlook.com>
2023-05-31 11:40:02 -0300

    feat(neovim): support autopairs

---
+++ roles/neovim/files/lua/config/autopairs.lua
@@ -0,0 +1,38 @@
+local M = {
+  "windwp/nvim-autopairs",
+  dependencies = { "hrsh7th/nvim-cmp" },
+}
+
+function M.opts()
+  return {
+    check_ts = true, -- treesitter integration
+    disable_filetype = { "TelescopePrompt" },
+    ts_config = {
+      lua = { "string", "source" },
+      javascript = { "string", "template_string" },
+      java = false,
+    },
+    fast_wrap = {
+      map = "<M-e>",
+      chars = { "{", "[", "(", '"', "'" },
+      pattern = string.gsub([[ [%'%"%)%>%]%)%}%,] ]], "%s+", ""),
+      offset = 0, -- Offset from pattern match
+      end_key = "$",
+      keys = "qwertyuiopzxcvbnmasdfghjkl",
+      check_comma = true,
+      highlight = "PmenuSel",
+      highlight_grey = "LineNr",
+    },
+  }
+end
+
+function M.config(_, opts)
+  require("nvim-autopairs").setup(opts)
+
+  local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+  local cmp = require("cmp")
+
+  cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done {})
+end
+
+return M
