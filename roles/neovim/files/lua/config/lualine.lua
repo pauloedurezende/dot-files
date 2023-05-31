192c94929fac06599222973c50e889e872ab24b7
Paulo Eduardo Rezende <rezendepauloeduardo@outlook.com>
2023-05-31 11:41:15 -0300

    refactor(neovim): improve lualine settings

--- roles/neovim/files/lua/config/lualine.lua
+++ roles/neovim/files/lua/config/lualine.lua
@@ -4,48 +4,77 @@ local M = {
 }
 
 function M.opts()
-  local function get_file_name()
-    if vim.bo.filetype == "NvimTree" then
-      return "  Explorer"
-    else
-      return " " .. vim.fn.expand "%:t"
+  local diagnostics = {
+    "diagnostics",
+    sources = { "nvim_diagnostic" },
+    sections = { "error", "warn" },
+    symbols = { error = " ", warn = " " },
+    colored = false,
+    update_in_insert = false,
+    always_visible = true,
+  }
+
+  local diff = {
+    "diff",
+    colored = false,
+    symbols = { added = " ", modified = " ", removed = " " }, -- changes diff symbols
+    cond = function()
+      return vim.fn.winwidth(0) > 80
     end
+  }
+
+  local mode = {
+    "mode",
+    fmt = function(str)
+      return "-- " .. str .. " --"
+    end,
+  }
+
+  local filetype = { "filetype", icons_enabled = false, icon = nil }
+
+  local branch = { "branch", icons_enabled = true, icon = "" }
+
+  local location = { "location", padding = 0 }
+
+  local function spaces()
+    return "spaces: " .. vim.api.nvim_buf_get_option(0, "shiftwidth")
+  end
+
+  local function progress()
+    local current_line = vim.fn.line(".")
+    local total_lines = vim.fn.line("$")
+    local chars = { "__", "▁▁", "▂▂", "▃▃", "▄▄", "▅▅", "▆▆", "▇▇", "██" }
+    local line_ratio = current_line / total_lines
+    local index = math.ceil(line_ratio * #chars)
+    return chars[index]
   end
 
   return {
     options = {
       icons_enabled = true,
       theme = "auto",
-      component_separators = { left = "", right = "" },
-      section_separators = { left = "", right = "" },
-      ignore_focus = {},
+      component_separators = { left = "", right = "" },
+      section_separators = { left = "", right = "" },
+      disabled_filetypes = { "alpha", "dashboard", "NvimTree", "Outline" },
       always_divide_middle = true,
-      globalstatus = true,
-      refresh = {
-        statusline = 1000,
-        tabline = 1000,
-        winbar = 1000,
-      },
     },
     sections = {
-      lualine_a = { "mode" },
-      lualine_b = { get_file_name },
+      lualine_a = { branch, diagnostics },
+      lualine_b = { mode },
       lualine_c = {},
-      lualine_x = {},
-      lualine_y = { "diagnostics" },
-      lualine_z = { "filetype" },
+      lualine_x = { diff, spaces, "encoding", filetype },
+      lualine_y = { location },
+      lualine_z = { progress },
     },
     inactive_sections = {
       lualine_a = {},
       lualine_b = {},
-      lualine_c = {},
-      lualine_x = {},
+      lualine_c = { "filename" },
+      lualine_x = { "location" },
       lualine_y = {},
       lualine_z = {},
     },
     tabline = {},
-    winbar = {},
-    inactive_winbar = {},
     extensions = {},
   }
 end
