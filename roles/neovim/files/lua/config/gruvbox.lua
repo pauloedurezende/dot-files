9dcdf2883caabc8964fb295e85cbafe31b64ad45
Paulo Eduardo Rezende <rezendepauloeduardo@outlook.com>
2023-05-31 11:40:36 -0300

    feat(neovim): support gruvbox theme

---
+++ roles/neovim/files/lua/config/gruvbox.lua
@@ -0,0 +1,14 @@
+local M = {
+  "sainnhe/gruvbox-material"
+}
+
+function M.config()
+  vim.cmd [[
+    let g:gruvbox_material_background = 'soft'
+    let g:gruvbox_material_foreground = "original"
+
+    colorscheme gruvbox-material
+  ]]
+end
+
+return M
