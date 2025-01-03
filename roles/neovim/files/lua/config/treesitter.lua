4ab2af19648777820c0d15c4498f1467d26cb88d
Paulo Eduardo Rezende <pauloeduardorezende@outlook.com>
2025-01-02 23:44:39 -0300

    feat(neovim): update lazy-lock.json dependencies and enhance treesitter configuration

--- roles/neovim/files/lua/config/treesitter.lua
+++ roles/neovim/files/lua/config/treesitter.lua
@@ -12,14 +12,10 @@ return {
 
     -- Define the configuration options for treesitter
     local opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'vim', 'vimdoc' },
-      -- Autoinstall languages that are not installed
+      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'vim', 'vimdoc', 'typescript' },
       auto_install = true,
       highlight = {
         enable = true,
-        -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-        -- If you are experiencing weird indenting issues, add the language to
-        -- the list of additional_vim_regex_highlighting and disabled languages for indent.
         additional_vim_regex_highlighting = { 'ruby' },
       },
       indent = { enable = true, disable = { 'ruby' } },
